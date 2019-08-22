f95612ffaf51ca69c93b6fad9ce0fc05ce5f270b
slisaasquatch <sli@saasquat.ch>
2019-08-22 12:49:48 -0700

    SL: thread dump utils

--- src/main/java/saasquatch/common/concurrent/RSThreads.java
+++ src/main/java/saasquatch/common/concurrent/RSThreads.java
@@ -1,6 +1,12 @@
 package saasquatch.common.concurrent;
 
+import java.lang.management.LockInfo;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MonitorInfo;
+import java.lang.management.ThreadInfo;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import saasquatch.common.base.RSThrowables;
@@ -27,6 +33,85 @@ public class RSThreads {
       logger.error("Error encountered when cleaning threadLocals", e);
       RSThrowables.wrapAndThrow(e);
     }
+  }
+
+  /**
+   * Get a <em>full</em> thread dump using {@link ManagementFactory#getThreadMXBean()}.
+   * @see #threadInfoToString(ThreadInfo)
+   */
+  public static String fullThreadDump() {
+    return Arrays.stream(ManagementFactory.getThreadMXBean().dumpAllThreads(true, true))
+        .map(RSThreads::threadInfoToString)
+        .collect(Collectors.joining());
+  }
+
+  /**
+   * Same as {@link ThreadInfo#toString()}, but removes the arbitrary stack trace limit.
+   */
+  public static String threadInfoToString(ThreadInfo threadInfo) {
+    StringBuilder sb = new StringBuilder("\"" + threadInfo.getThreadName() + "\"" + " Id="
+        + threadInfo.getThreadId() + " " + threadInfo.getThreadState());
+    if (threadInfo.getLockName() != null) {
+      sb.append(" on " + threadInfo.getLockName());
+    }
+    if (threadInfo.getLockOwnerName() != null) {
+      sb.append(
+          " owned by \"" + threadInfo.getLockOwnerName() + "\" Id=" + threadInfo.getLockOwnerId());
+    }
+    if (threadInfo.isSuspended()) {
+      sb.append(" (suspended)");
+    }
+    if (threadInfo.isInNative()) {
+      sb.append(" (in native)");
+    }
+    sb.append('\n');
+    int i = 0;
+    for (; i < threadInfo.getStackTrace().length /* && i < MAX_FRAMES */; i++) {
+      StackTraceElement ste = threadInfo.getStackTrace()[i];
+      sb.append("\tat " + ste.toString());
+      sb.append('\n');
+      if (i == 0 && threadInfo.getLockInfo() != null) {
+        Thread.State ts = threadInfo.getThreadState();
+        switch (ts) {
+          case BLOCKED:
+            sb.append("\t-  blocked on " + threadInfo.getLockInfo());
+            sb.append('\n');
+            break;
+          case WAITING:
+            sb.append("\t-  waiting on " + threadInfo.getLockInfo());
+            sb.append('\n');
+            break;
+          case TIMED_WAITING:
+            sb.append("\t-  waiting on " + threadInfo.getLockInfo());
+            sb.append('\n');
+            break;
+          default:
+        }
+      }
+
+      for (MonitorInfo mi : threadInfo.getLockedMonitors()) {
+        if (mi.getLockedStackDepth() == i) {
+          sb.append("\t-  locked " + mi);
+          sb.append('\n');
+        }
+      }
+    }
+    if (i < threadInfo.getStackTrace().length) {
+      sb.append("\t...");
+      sb.append('\n');
+    }
+
+    LockInfo[] locks = threadInfo.getLockedSynchronizers();
+    if (locks.length > 0) {
+      sb.append("\n\tNumber of locked synchronizers = " + locks.length);
+      sb.append('\n');
+      for (LockInfo li : locks) {
+        sb.append("\t- " + li);
+        sb.append('\n');
+      }
+    }
+    sb.append('\n');
+    return sb.toString();
   }
 
 }
