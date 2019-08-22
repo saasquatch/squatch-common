61766f102109a7162e6c3098b81afd35840d58a9
slisaasquatch <sli@saasquat.ch>
2019-08-22 12:21:19 -0700

    SL: test optimization

--- src/test/java/saasquatch/common/base/RSStringsTests.java
+++ src/test/java/saasquatch/common/base/RSStringsTests.java
@@ -13,8 +13,9 @@ public class RSStringsTests {
       final String s = RandomStringUtils.random(1024);
       assertTrue(s.getBytes(UTF_8).length > 512);
       String truncated = RSStrings.truncateToUtf8ByteSize(s, 512);
-      assertTrue("We should never exceed the limit", truncated.getBytes(UTF_8).length <= 512);
-      assertTrue("We should be at most 4 bytes off", truncated.getBytes(UTF_8).length > 512 - 4);
+      final int truncatedUtf8Size = truncated.getBytes(UTF_8).length;
+      assertTrue("We should never exceed the limit", truncatedUtf8Size <= 512);
+      assertTrue("We should be at most 4 bytes off", truncatedUtf8Size > 512 - 4);
     }
   }
 
