56811367140bcd3ac3c327d64fdee6acaaf8048e
slisaasquatch <sli@saasquat.ch>
2019-08-29 09:07:07 -0700

    unit test for decodeLenient

--- src/test/java/saasquatch/common/base/RSUrlCodecTest.java
+++ src/test/java/saasquatch/common/base/RSUrlCodecTest.java
@@ -31,6 +31,7 @@ public class RSUrlCodecTest {
         + "%2F%20%3A%20%3B%20%3D%20%3F%20%40%20%5B%20%5D";
     assertEquals(expected, RSUrlCodec.encodeStandard(reserved));
     assertEquals(reserved, RSUrlCodec.decode(expected));
+    assertEquals(reserved, RSUrlCodec.decodeLenient(expected));
     assertEquals(reserved, URLDecoder.decode(expected, UTF_8.name()));
   }
 
@@ -43,6 +44,9 @@ public class RSUrlCodecTest {
       assertEquals(original, RSUrlCodec.decode(RSUrlCodec.encodeStandard(original)));
       assertEquals(original, RSUrlCodec.decode(RSUrlCodec.encode(original, pred, false)));
       assertEquals(original, RSUrlCodec.decode(RSUrlCodec.encode(original, pred, true)));
+      assertEquals(original, RSUrlCodec.decodeLenient(RSUrlCodec.encodeStandard(original)));
+      assertEquals(original, RSUrlCodec.decodeLenient(RSUrlCodec.encode(original, pred, false)));
+      assertEquals(original, RSUrlCodec.decodeLenient(RSUrlCodec.encode(original, pred, true)));
       assertEquals(original,
           URLDecoder.decode(RSUrlCodec.encode(original, pred, false), UTF_8.name()));
       assertEquals(original,
@@ -62,6 +66,7 @@ public class RSUrlCodecTest {
     });
     reservedMappings.forEach((k, v) -> {
       assertEquals(k, RSUrlCodec.decode(v));
+      assertEquals(k, RSUrlCodec.decodeLenient(v));
       try {
         assertEquals(k, URLDecoder.decode(v, UTF_8.name()));
       } catch (UnsupportedEncodingException e) {
@@ -80,6 +85,7 @@ public class RSUrlCodecTest {
         "%3Ch2%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Percent-encoding_in_a_URI%22%3EPercent-encoding%20in%20a%20URI%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D1%22%20title%3D%22Edit%20section%3A%20Percent-encoding%20in%20a%20URI%22%3Eedit%3C%2Fa%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh2%3E%0D%0A%3Ch3%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Types_of_URI_characters%22%3ETypes%20of%20URI%20characters%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D2%22%20title%3D%22Edit%20section%3A%20Types%20of%20URI%20characters%22%3Eedit%3C%2Fa%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh3%3E%0D%0A%3Cp%3EThe%20characters%20allowed%20in%20a%20URI%20are%20either%20%3Ci%3Ereserved%3C%2Fi%3E%20or%20%3Ci%3Eunreserved%3C%2Fi%3E%20%28or%20a%20percent%20character%20as%20part%20of%20a%20percent-encoding%29.%20%3Ci%3EReserved%3C%2Fi%3E%20characters%20are%20those%20characters%20that%20sometimes%20have%20special%20meaning.%20For%20example%2C%20%3Ca%20href%3D%22%2Fwiki%2FForward_slash%22%20class%3D%22mw-redirect%22%20title%3D%22Forward%20slash%22%3Eforward%20slash%3C%2Fa%3E%20characters%20are%20used%20to%20separate%20different%20parts%20of%20a%20URL%20%28or%20more%20generally%2C%20a%20URI%29.%20%3Ci%3EUnreserved%3C%2Fi%3E%20characters%20have%20no%20such%20meanings.%20Using%20percent-encoding%2C%20reserved%20characters%20are%20represented%20using%20special%20character%20sequences.%20The%20sets%20of%20reserved%20and%20unreserved%20characters%20and%20the%20circumstances%20under%20which%20certain%20reserved%20characters%20have%20special%20meaning%20have%20changed%20slightly%20with%20each%20revision%20of%20specifications%20that%20govern%20URIs%20and%20URI%20schemes.%3C%2Fp%3E";
     assertEquals(encoded, RSUrlCodec.encodeStandard(original));
     assertEquals(original, RSUrlCodec.decode(encoded));
+    assertEquals(original, RSUrlCodec.decodeLenient(encoded));
     assertEquals(original, URLDecoder.decode(encoded, UTF_8.name()));
   }
 
