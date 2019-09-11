16a7fa4609eb86d5355f533004d6e035363c4f51
slisaasquatch <sli@saasquat.ch>
2019-09-11 09:38:46 -0700

    wrap long strings

--- src/test/java/saasquatch/common/base/RSUrlCodecTest.java
+++ src/test/java/saasquatch/common/base/RSUrlCodecTest.java
@@ -95,11 +95,53 @@ public class RSUrlCodecTest {
   @Test
   public void testRealHtml() throws Exception {
     final String original =
-        "<h2><span class=\"mw-headline\" id=\"Percent-encoding_in_a_URI\">Percent-encoding in a URI</span><span class=\"mw-editsection\"><span class=\"mw-editsection-bracket\">[</span><a href=\"/w/index.php?title=Percent-encoding&amp;action=edit&amp;section=1\" title=\"Edit section: Percent-encoding in a URI\">edit</a><span class=\"mw-editsection-bracket\">]</span></span></h2>\r\n"
-            + "<h3><span class=\"mw-headline\" id=\"Types_of_URI_characters\">Types of URI characters</span><span class=\"mw-editsection\"><span class=\"mw-editsection-bracket\">[</span><a href=\"/w/index.php?title=Percent-encoding&amp;action=edit&amp;section=2\" title=\"Edit section: Types of URI characters\">edit</a><span class=\"mw-editsection-bracket\">]</span></span></h3>\r\n"
-            + "<p>The characters allowed in a URI are either <i>reserved</i> or <i>unreserved</i> (or a percent character as part of a percent-encoding). <i>Reserved</i> characters are those characters that sometimes have special meaning. For example, <a href=\"/wiki/Forward_slash\" class=\"mw-redirect\" title=\"Forward slash\">forward slash</a> characters are used to separate different parts of a URL (or more generally, a URI). <i>Unreserved</i> characters have no such meanings. Using percent-encoding, reserved characters are represented using special character sequences. The sets of reserved and unreserved characters and the circumstances under which certain reserved characters have special meaning have changed slightly with each revision of specifications that govern URIs and URI schemes.</p>";
+        "<h2><span class=\"mw-headline\" id=\"Percent-encoding_in_a_URI\">Percent-encoding in a "
+            + "URI</span><span class=\"mw-editsection\"><span class=\"mw-editsection-bracket\">["
+            + "</span><a href=\"/w/index.php?title=Percent-encoding&amp;action=edit&amp;section=1\""
+            + " title=\"Edit section: Percent-encoding in a URI\">edit</a><span class=\"mw-editsection"
+            + "-bracket\">]</span></span></h2>\r\n"
+            + "<h3><span class=\"mw-headline\" id=\"Types_of_URI_characters\">Types of URI "
+            + "characters</span><span class=\"mw-editsection\"><span class=\"mw-editsection-"
+            + "bracket\">[</span><a href=\"/w/index.php?title=Percent-encoding&amp;action=edit"
+            + "&amp;section=2\" title=\"Edit section: Types of URI characters\">edit</a><span "
+            + "class=\"mw-editsection-bracket\">]</span></span></h3>\r\n"
+            + "<p>The characters allowed in a URI are either <i>reserved</i> or <i>unreserved"
+            + "</i> (or a percent character as part of a percent-encoding). <i>Reserved</i> "
+            + "characters are those characters that sometimes have special meaning. For example"
+            + ", <a href=\"/wiki/Forward_slash\" class=\"mw-redirect\" title=\"Forward slash\">"
+            + "forward slash</a> characters are used to separate different parts of a URL (or "
+            + "more generally, a URI). <i>Unreserved</i> characters have no such meanings. "
+            + "Using percent-encoding, reserved characters are represented using special "
+            + "character sequences. The sets of reserved and unreserved characters and the "
+            + "circumstances under which certain reserved characters have special meaning have"
+            + " changed slightly with each revision of specifications that govern URIs and URI"
+            + " schemes.</p>";
     final String encoded =
-        "%3Ch2%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Percent-encoding_in_a_URI%22%3EPercent-encoding%20in%20a%20URI%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D1%22%20title%3D%22Edit%20section%3A%20Percent-encoding%20in%20a%20URI%22%3Eedit%3C%2Fa%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh2%3E%0D%0A%3Ch3%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Types_of_URI_characters%22%3ETypes%20of%20URI%20characters%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D2%22%20title%3D%22Edit%20section%3A%20Types%20of%20URI%20characters%22%3Eedit%3C%2Fa%3E%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh3%3E%0D%0A%3Cp%3EThe%20characters%20allowed%20in%20a%20URI%20are%20either%20%3Ci%3Ereserved%3C%2Fi%3E%20or%20%3Ci%3Eunreserved%3C%2Fi%3E%20%28or%20a%20percent%20character%20as%20part%20of%20a%20percent-encoding%29.%20%3Ci%3EReserved%3C%2Fi%3E%20characters%20are%20those%20characters%20that%20sometimes%20have%20special%20meaning.%20For%20example%2C%20%3Ca%20href%3D%22%2Fwiki%2FForward_slash%22%20class%3D%22mw-redirect%22%20title%3D%22Forward%20slash%22%3Eforward%20slash%3C%2Fa%3E%20characters%20are%20used%20to%20separate%20different%20parts%20of%20a%20URL%20%28or%20more%20generally%2C%20a%20URI%29.%20%3Ci%3EUnreserved%3C%2Fi%3E%20characters%20have%20no%20such%20meanings.%20Using%20percent-encoding%2C%20reserved%20characters%20are%20represented%20using%20special%20character%20sequences.%20The%20sets%20of%20reserved%20and%20unreserved%20characters%20and%20the%20circumstances%20under%20which%20certain%20reserved%20characters%20have%20special%20meaning%20have%20changed%20slightly%20with%20each%20revision%20of%20specifications%20that%20govern%20URIs%20and%20URI%20schemes.%3C%2Fp%3E";
+        "%3Ch2%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Percent-encoding_in_a_URI%22%3E"
+            + "Percent-encoding%20in%20a%20URI%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E"
+            + "%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw"
+            + "%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D1%22%20"
+            + "title%3D%22Edit%20section%3A%20Percent-encoding%20in%20a%20URI%22%3Eedit%3C%2Fa%3E%3C"
+            + "span%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh2%3E"
+            + "%0D%0A%3Ch3%3E%3Cspan%20class%3D%22mw-headline%22%20id%3D%22Types_of_URI_characters%22"
+            + "%3ETypes%20of%20URI%20characters%3C%2Fspan%3E%3Cspan%20class%3D%22mw-editsection%22%3E"
+            + "%3Cspan%20class%3D%22mw-editsection-bracket%22%3E%5B%3C%2Fspan%3E%3Ca%20href%3D%22%2Fw"
+            + "%2Findex.php%3Ftitle%3DPercent-encoding%26amp%3Baction%3Dedit%26amp%3Bsection%3D2%22%20"
+            + "title%3D%22Edit%20section%3A%20Types%20of%20URI%20characters%22%3Eedit%3C%2Fa%3E%3Cspan"
+            + "%20class%3D%22mw-editsection-bracket%22%3E%5D%3C%2Fspan%3E%3C%2Fspan%3E%3C%2Fh3%3E%0D"
+            + "%0A%3Cp%3EThe%20characters%20allowed%20in%20a%20URI%20are%20either%20%3Ci%3Ereserved%3C"
+            + "%2Fi%3E%20or%20%3Ci%3Eunreserved%3C%2Fi%3E%20%28or%20a%20percent%20character%20as%20part"
+            + "%20of%20a%20percent-encoding%29.%20%3Ci%3EReserved%3C%2Fi%3E%20characters%20are%20those"
+            + "%20characters%20that%20sometimes%20have%20special%20meaning.%20For%20example%2C%20%3Ca"
+            + "%20href%3D%22%2Fwiki%2FForward_slash%22%20class%3D%22mw-redirect%22%20title%3D%22Forward"
+            + "%20slash%22%3Eforward%20slash%3C%2Fa%3E%20characters%20are%20used%20to%20separate%20"
+            + "different%20parts%20of%20a%20URL%20%28or%20more%20generally%2C%20a%20URI%29.%20%3Ci%3E"
+            + "Unreserved%3C%2Fi%3E%20characters%20have%20no%20such%20meanings.%20Using%20percent-"
+            + "encoding%2C%20reserved%20characters%20are%20represented%20using%20special%20character"
+            + "%20sequences.%20The%20sets%20of%20reserved%20and%20unreserved%20characters%20and%20the"
+            + "%20circumstances%20under%20which%20certain%20reserved%20characters%20have%20special%20"
+            + "meaning%20have%20changed%20slightly%20with%20each%20revision%20of%20specifications%20"
+            + "that%20govern%20URIs%20and%20URI%20schemes.%3C%2Fp%3E";
     assertEquals(encoded, RSUrlCodec.encode(original));
     assertEquals(original, RSUrlCodec.decode(encoded));
     assertEquals(original, RSUrlCodec.getLenientDecoder().decode(encoded));
@@ -132,15 +174,21 @@ public class RSUrlCodecTest {
       final String toEncode =
           "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">";
       assertEquals(
-          "%3C%68%74%6D%6C+%78%6D%6C%6E%73%3D%22%68%74%74%70%3A%2F%2F%77%77%77%2E%77%33%2E%6F%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22+%78%6D%6C%3A%6C%61%6E%67%3D%22%65%6E%22+%6C%61%6E%67%3D%22%65%6E%22%3E",
+          "%3C%68%74%6D%6C+%78%6D%6C%6E%73%3D%22%68%74%74%70%3A%2F%2F%77%77%77%2E%77%33%2E"
+              + "%6F%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22+%78%6D%6C%3A%6C%61%6E%67%3D%22"
+              + "%65%6E%22+%6C%61%6E%67%3D%22%65%6E%22%3E",
           RSUrlCodec.getEncoder().withSafeCharPredicate(_i -> false).encodeSpaceToPlus(true)
               .encode(toEncode));
       assertEquals(
-          "%3C%68%74%6D%6C%20%78%6D%6C%6E%73%3D%22%68%74%74%70%3A%2F%2F%77%77%77%2E%77%33%2E%6F%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22%20%78%6D%6C%3A%6C%61%6E%67%3D%22%65%6E%22%20%6C%61%6E%67%3D%22%65%6E%22%3E",
+          "%3C%68%74%6D%6C%20%78%6D%6C%6E%73%3D%22%68%74%74%70%3A%2F%2F%77%77%77%2E%77%33"
+              + "%2E%6F%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22%20%78%6D%6C%3A%6C%61%6E%67%3D"
+              + "%22%65%6E%22%20%6C%61%6E%67%3D%22%65%6E%22%3E",
           RSUrlCodec.getEncoder().withSafeCharPredicate(_i -> false).encodeSpaceToPlus(false)
               .encode(toEncode));
       assertEquals(
-          "<%68%74%6D%6C%20%78%6D%6C%6E%73=%22%68%74%74%70:%2F%2F%77%77%77%2E%77%33%2E%6F%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22%20%78%6D%6C:%6C%61%6E%67=%22%65%6E%22%20%6C%61%6E%67=%22%65%6E%22>",
+          "<%68%74%6D%6C%20%78%6D%6C%6E%73=%22%68%74%74%70:%2F%2F%77%77%77%2E%77%33%2E%6F"
+              + "%72%67%2F%31%39%39%39%2F%78%68%74%6D%6C%22%20%78%6D%6C:%6C%61%6E%67=%22%65%6E"
+              + "%22%20%6C%61%6E%67=%22%65%6E%22>",
           RSUrlCodec.getEncoder()
               .withSafeCharPredicate(_i -> _i == '=' || _i == ':' || _i == '<' || _i == '>')
               .encode(toEncode));
