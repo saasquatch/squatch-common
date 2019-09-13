606cffd8d8b9fc22d97e905a9239e63f60f1c0fb
slisaasquatch <sli@saasquat.ch>
2019-09-13 09:37:07 -0700

    Create RSExecutors.java

---
+++ src/main/java/saasquatch/common/concurrent/RSExecutors.java
@@ -0,0 +1,43 @@
+package saasquatch.common.concurrent;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Utilities for {@link Executor}s.
+ *
+ * @author sli
+ */
+public final class RSExecutors {
+
+  private RSExecutors() {}
+
+  /**
+   * {@link Executor} where a new {@link Thread} is created per task.
+   *
+   * @return a singleton {@link Executor}
+   */
+  public static Executor threadPerTaskExecutor(boolean daemon) {
+    return daemon ? TPTExecutor.DAEMON : TPTExecutor.NON_DAEMON;
+  }
+
+  private static enum TPTExecutor implements Executor {
+    DAEMON(true), NON_DAEMON(false),;
+
+    private final boolean daemon;
+    private final ThreadGroup threadGroup;
+
+    private TPTExecutor(boolean daemon) {
+      this.daemon = daemon;
+      this.threadGroup = new ThreadGroup(String.format("%s.threadPerTaskExecutor (%sdaemon)",
+          RSExecutors.class.getSimpleName(), daemon ? "" : "non-"));
+    }
+
+    @Override
+    public void execute(Runnable command) {
+      final Thread t = new Thread(threadGroup, command);
+      t.setDaemon(daemon);
+      t.start();
+    }
+  }
+
+}
