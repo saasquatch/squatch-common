9a9db4c7d364bd067880f889b38af7c4f808cffa
slisaasquatch <sli@saasquat.ch>
2019-10-25 08:54:39 -0700

    javadoc update

--- src/main/java/com/saasquatch/common/concurrent/RSThreadLocalSecureRandom.java
+++ src/main/java/com/saasquatch/common/concurrent/RSThreadLocalSecureRandom.java
@@ -5,8 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 import javax.annotation.Nonnull;
 
 /**
- * Similar to {@link ThreadLocalRandom}, but returns {@link SecureRandom}s created by
- * {@link #newFastSecureRandom()}.
+ * Similar to {@link ThreadLocalRandom}, but returns {@link SecureRandom}s. This class by default
+ * does not use the default constructor of {@link SecureRandom}. Instead, it tries to create
+ * <em>potentially faster</em> {@link SecureRandom} by trying faster algorithms and fallback to the
+ * default constructor.
  *
  * @author sli
  */
@@ -30,8 +32,6 @@ public final class RSThreadLocalSecureRandom {
    * Attempt to create a {@link SecureRandom} with the algorithms in the given order, or fallback to
    * {@link SecureRandom#SecureRandom() new SecureRandom()} if none of the attempted algorithms is
    * available.
-   *
-   * @see #current()
    */
   // Visible for testing
   static SecureRandom newSecureRandomForAlgorithms(String... preferredAlgorithms) {
